f0bf11cb1394908f45b3500fd5646818c4d9bd8a
Jason Wiram <jwiram@gmail.com>
2018-11-26 20:36:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget() {
 function increaseRankBy(n) {
   let myList = document.querySelectorAll('ul li')
   for (let i = 0; i < myList.length; i++) {
-    let current = myList[i].innerHTML
+    let current = parseInt(myList[i].innerHTML)
     myList[i].innerHTML = (i + n).toString();
   }
 }
