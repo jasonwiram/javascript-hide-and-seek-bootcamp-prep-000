a0323cbdb6cc3ea4423773c6fcbf60b78728a293
Jason Wiram <jwiram@gmail.com>
2018-11-26 21:31:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector("#nested .target")
+}
+
+function increaseRankBy(n) {
+  let myList = document.querySelectorAll('ul li')
+  for (let i = 0; i < myList.length; i++) {
+    let current = parseInt(myList[i].innerHTML)
+    myList[i].innerHTML = (current + n).toString();
+  }
+}
+
+function deepestChild() {
+  let deep = document.getElementById('grand-node').querySelectorAll('div')
+  return deep[deep.length-1]
+}
