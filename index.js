ac7fca1b6cd0f39fb3915ca4c19d3f8c06bccc53
Jason Wiram <jwiram@gmail.com>
2018-11-26 21:29:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  let deepestNode = document.getElementById('grand-node').querySelectorAll('div')
+  let nodes = document.getElementById('grand-node').querySelectorAll('div')
   return deepestNode[deep.length-1]
 }
