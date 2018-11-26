792e4096de046efff9f067d94a48dfd88f1f0115
Jason Wiram <jwiram@gmail.com>
2018-11-26 20:36:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function increaseRankBy(n) {
   let myList = document.querySelectorAll('ul li')
   for (let i = 0; i < myList.length; i++) {
     let current = parseInt(myList[i].innerHTML)
-    myList[i].innerHTML = (i + n).toString();
+    myList[i].innerHTML = (current + n).toString();
   }
 }
 
