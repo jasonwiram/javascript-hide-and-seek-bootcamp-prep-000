9f227027c74c33af091b2c1d627830ad7bf7bc3a
Jason Wiram <jwiram@gmail.com>
2018-11-26 19:34:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function getFirstSelector(selector) {
   return document.querySelector(selector)
 }
+
+function nestedTarget() {
+  
+}
+
+function increaseRankBy(n) {
+  
+}
+
+function deepestChild() {
+  
+}
