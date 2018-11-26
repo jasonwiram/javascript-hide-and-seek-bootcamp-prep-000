09ea1000454fc34b2e0863357273ec021ac9122a
Jason Wiram <jwiram@gmail.com>
2018-11-26 21:22:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  let grandeNode = document.querySelectorAll('#grande-node *')
+  let grandeNode = document.querySelectorAll('#grande-node')
   
 }
